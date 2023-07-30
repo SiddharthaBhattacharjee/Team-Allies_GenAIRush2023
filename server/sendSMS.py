48496a5ba21bc8089283e7ace185a4c181168594
Siddhartha Bhattacharjee <siddharthabhatt2345@gmail.com>
2023-07-30 08:10:59 +0530

    Update sendSMS.py

--- server/sendSMS.py
+++ server/sendSMS.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 def sendMSG(msg, ph_no) :
-    account_sid = "ACb8b05c3a3b447064a4dc61795ef83455"
-    auth_token = "7729c783fd989ba8e62c04bac6aa642d"
+    account_sid = "ACb8b05c3a3b447064a4dc61795exxxxx"
+    auth_token = "7729c783fd989ba8e62c04bac6axxxxx"
     client = Client(account_sid, auth_token)
     message = client.messages.create(
       body=msg,
